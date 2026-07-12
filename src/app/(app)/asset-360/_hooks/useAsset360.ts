961ef4609686668d81cf19517085becce6f29b1d
hoang12122 <81763220+hoang12122@users.noreply.github.com>
2026-07-12 13:22:42 +0700

    fix(lint): remove stale exhaustive-deps directive

--- src/app/(app)/asset-360/_hooks/useAsset360.ts
+++ src/app/(app)/asset-360/_hooks/useAsset360.ts
@@ -23,7 +23,6 @@ export const useAsset360 = () => {
 
     useEffect(() => {
         loadEquipments();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const handleSelectEquipment = async (id: string) => {
