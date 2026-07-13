9a4e2f2a27c172fde8573c01eefd09585e760188
hoang12122 <81763220+hoang12122@users.noreply.github.com>
2026-07-13 18:16:52 +0700

    fix(types): remove pg declaration that shadowed official types

--- src/types/pg.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'pg' {
-    export class Client {
-        constructor(config?: any);
-        connect(): Promise<void>;
-        on(event: string, listener: (...args: any[]) => void): this;
-        query(queryText: string, values?: any[]): Promise<any>;
-        end(): Promise<void>;
-    }
-}
